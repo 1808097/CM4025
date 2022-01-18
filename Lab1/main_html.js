116449c3b537fe2559b0ba9dd4c291714ed1a844
ANDRAS SARDI (1808097) <a.sardi@rgu.ac.uk>
2022-01-18 16:46:29 +0000

    Changing path line

--- Lab1/main_html.js
+++ Lab1/main_html.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'content-type': 'text/html'})
-    fs.createReadStream('Lab1.html').pipe(res)
+    fs.createReadStream('Lab1/Lab1.html').pipe(res)
 })
 
 server.listen(process.env.PORT || 3000)
